b6b6dd5eed664b7fd180a2c64d60049dd3c44773
Trygve <trygve@provingground.io>
2020-03-22 21:22:26 -0500

    fix: query errors

--- server/db/migrations/2.2.4.js
+++ server/db/migrations/2.2.4.js
@@ -1,15 +1,17 @@
-/* global WIKI */
-
 exports.up = knex => {
   return knex.schema
     .createTable('features', table => {
       table.increments('id').primary()
       table.integer('parentId')
       table.string('title').notNullable()
+      table.string('description')
       table.jsonb('geojson').notNullable()
       table.string('createdAt').notNullable()
       table.string('updatedAt').notNullable()
     })
 }
 
-exports.down = knex => { }
+exports.down = knex => {
+  return knex.schema
+    .dropTable('features')
+}
